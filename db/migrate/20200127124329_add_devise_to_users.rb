3c4356ad5cfa78be734e60f857e30c4e943ad8e1
wada-riki <60034781+wada-riki@users.noreply.github.com>
2020-01-28 18:24:40 +0900

    group編集

--- db/migrate/20200127124329_add_devise_to_users.rb
+++ db/migrate/20200127124329_add_devise_to_users.rb
@@ -4,6 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, unique: true, index: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
